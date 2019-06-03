15f3b67ee8a155eebffdfc585e2df0123be84739
Maciej Żygowski <mzygowski@hotmail.com>
2019-06-03 07:18:22 +0200

    Działa oraz zmiany do zaliczenia przedmiotu.

--- 20_pytan/main.cpp
+++ 20_pytan/main.cpp
@@ -3,12 +3,48 @@
 #include <string>
 #include "baza_danych.h"
 #include <conio.h>
-	
+#include <fstream>
+#include <cstdlib>
+
+#define CHAR_IN_LINE  60
+
 void zamiana(std::string*);
 
 int main()
 {
 	SetConsoleOutputCP(CP_UTF8);
+	std::fstream  pytania;
+	pytania.open("pytania.txt",std::ios::in);
+	if (pytania.good() == true)
+	{
+		std::string li;
+		int linie = 0;
+		
+		while(getline(pytania, li))
+		{
+			std::cout << li << "\n\n";
+			linie++;
+		}
+		char *question_linie = (char *)malloc(linie * CHAR_IN_LINE * sizeof(char));
+		pytania.close();
+		pytania.open("pytania.txt", std::ios::in);
+		for (int i = 0; i < linie; i++)
+		{
+			for (int j = 0; j < CHAR_IN_LINE; j++)
+			{
+				pytania>>question_linie[i*CHAR_IN_LINE+j];
+				if (question_linie[i*CHAR_IN_LINE + j] == '\n' || question_linie[i*CHAR_IN_LINE + j] == EOF)
+				{
+					break;
+				}
+			}
+		}
+		pytania.close();
+	}
+	else
+	{
+		std::cout << "B³¹d oczytywania pytañ";
+	}
 	baza_danych baza;
 	if (baza.connect())
 	{
@@ -19,6 +55,10 @@ int main()
 			query = baza.gen_query(1);
 			query = baza.choice(query);
 			baza.answers[0][i] = query;
+			if (baza.answers[0][i] == "")
+			{
+				break;
+			}
 			query = "SELECT pytanie FROM question WHERE id = " + query;
 			row = mysql_fetch_row(baza.zapytanie(query));
 			if (baza.all_wyrazy == 1)
@@ -34,11 +74,33 @@ int main()
 			zamiana(&baza.answers[1][i]);
 			baza.count_answers++;
 		}
+		query = "SELECT slowo FROM relacje,(";
+		query += baza.gen_baza_query(1);
+		query += ") AS words WHERE words.id = relacje.id_words";
+		row = mysql_fetch_row(baza.zapytanie(query));
+		std::cout << "\n\nCzy twoje s³owo to:  " << row[0] << "\n\n";
+		std::cout << "Tak(y/Y) Nie(n/N)";
+		std::string ostateczna;
+		ostateczna = _getch();
+		zamiana(&ostateczna);
+		system("cls");
+		if (ostateczna == "0")
+		{
+			std::cout << "Dziekuje za gre!\n";
+		}
+		else
+		{
+			std::cout << "O jakim slowie myslales?\n";
+			std::string slowo;
+			std::cin >> slowo;
+		}
+			
 	}
 	else
 	{
 		puts("Po³¹czenie nie udane");
 	}
+	
 	baza.~baza_danych();
 	system("PAUSE");
 	std::cin.get();
